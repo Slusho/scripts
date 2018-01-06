2b16bd9a342cd5b16d2bd86a37b2b4960029da44
Willie Thornton <willracerx@gmail.com>
2018-01-06 11:23:55 -0600

    Fixed color of bar graph to scale with width.

--- Get-DiskStatus.ps1
+++ Get-DiskStatus.ps1
@@ -2,14 +2,14 @@
 Begin {
     function Get-BarGraph($Percent){
         # Max width of graph
-        $Width = 30
+        $Width = 20
         Write-Host '['-NoNewline
         # Loop that animates the bar also scales the percentage so bar does not take up too much space
-        While($tick++ -le $Percent/100*30-1){  
-            if ($tick -le 18) {
-                write-Host "$([char]0x25A0)" -NoNewline -ForegroundColor Green
+        While($tick++ -le $Percent/100*$Width-1){  
+            if ($tick -le $Width / 1.7) {
+                Write-Host "$([char]0x25A0)" -NoNewline -ForegroundColor Green
             }
-            elseif ($tick -le 27) {
+            elseif ($tick -le $Width / 1.1) {
                 Write-Host "$([char]0x25A0)" -NoNewline -ForegroundColor Yellow
             }
             else {
@@ -19,7 +19,7 @@ Begin {
             Start-Sleep -Milliseconds 5
         }
         # Calculates how much free space at end of the graph
-        $spaceLeft = ((1)*30) - $tick
+        $spaceLeft = ((1)*$Width) - $tick
         # Loop draws empty space and end of graph
         While($add++ -le $spaceLeft){
             Write-Host ' ' -NoNewline
@@ -33,27 +33,25 @@ Begin {
 
 Process {
     $myDisk | ForEach-Object {
-        if ($_.Drivetype -ne 4){
-            echo 'test'
-
-        $TotalSpace = [Math]::Round($_.Size / 1gb)
-        $FreeSpace = [Math]::Round($_.Freespace / 1gb)
-        $UsedSpace = [Math]::Round(($_.Size - $_.FreeSpace) / 1gb)
-        $PercentUsed = [Math]::Round((($_.Size - $_.FreeSpace) / $_.Size) * 100)
-        $PercentFree = [Math]::Round(($_.FreeSpace / $_.Size) * 100)
-        
-        $param = @{
-            Volume = $_.VolumeName
-            DriveLetter = $_.DeviceID
-            TotalSpace = "$TotalSpace GB"
-            UsedSpace = "$UsedSpace GB"
-            FreeSpace = "$FreeSpace GB"
-            PercentUsed = "$PercentUsed %"
-            PercentFree = "$PercentFree %"
+        if ($_.Drivetype -ne 4) {
+            $TotalSpace = [Math]::Round($_.Size / 1gb)
+            $FreeSpace = [Math]::Round($_.Freespace / 1gb)
+            $UsedSpace = [Math]::Round(($_.Size - $_.FreeSpace) / 1gb)
+            $PercentUsed = [Math]::Round((($_.Size - $_.FreeSpace) / $_.Size) * 100)
+            $PercentFree = [Math]::Round(($_.FreeSpace / $_.Size) * 100)
+            
+            $param = @{
+                Volume = $_.VolumeName
+                DriveLetter = $_.DeviceID
+                TotalSpace = "$TotalSpace GB"
+                UsedSpace = "$UsedSpace GB"
+                FreeSpace = "$FreeSpace GB"
+                PercentUsed = "$PercentUsed %"
+                PercentFree = "$PercentFree %"
+            }
+            $Results = New-Object -TypeName PSObject -Property $param
+            $Results | ft
+            Get-BarGraph $Results.PercentUsed.Replace('%', '')
         }
-        $Results = New-Object -TypeName PSObject -Property $param
-        $Results | ft
-        Get-BarGraph $Results.PercentUsed.Replace('%', '')
-    }
     }
 }
