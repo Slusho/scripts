21883c334babd2a8475acd137cebad8eca0bfaa3
Willie Thornton <willracerx@gmail.com>
2018-01-06 10:40:21 -0600

    Added changes per suggested from reddit/r/powershell users. Placed function at top of document. Made width of graph a variable. Added color markers to graph.

--- Get-DiskStatus.ps1
+++ Get-DiskStatus.ps1
@@ -1,49 +1,59 @@
 ﻿# Run this script to see connected hard disk status with nifty bar graph!
-
-$myDisk = Get-WmiObject -class win32_logicaldisk
-$myDisk | ForEach-Object {
-    $free = ($_.freespace)
-    $total = ($_.size)
-    $used = $total - $free
-    $perUsed = ($used / $total) * 100
-    $perFree = ($free / $total) * 100
-    $t = [math]::Round($total/1gb)
-    $f = [math]::Round($free/1gb)
-    $u = [math]::Round($used/1gb)
-    $pu = [math]::Round($perUsed)
-    $pf = [math]::Round($perFree)
-    
-    write-host "Volume:    "$_.VolumeName
-    write-host "DeviceID:  "$_.DeviceID
-    echo "Total:      $t/Gb"
-    echo "Used:       $u/Gb"
-    echo "Free:       $f/Gb"
-    echo "% used:     $pu%"
-    echo "% free:     $pf%"
-    
-    # Create bar graph
-    function drawBar{
-        # Tracks how many ticks to draw
-        $tick
+Begin {
+    function Get-BarGraph($Percent){
+        # Max width of graph
+        $Width = 30
         Write-Host '['-NoNewline
         # Loop that animates the bar also scales the percentage so bar does not take up too much space
-        While($tick++ -le $pu/100*30-1){  
-            write-Host "$([char]0x25A0)" -NoNewline
+        While($tick++ -le $Percent/100*30-1){  
+            if ($tick -le 18) {
+                write-Host "$([char]0x25A0)" -NoNewline -ForegroundColor Green
+            }
+            elseif ($tick -le 27) {
+                Write-Host "$([char]0x25A0)" -NoNewline -ForegroundColor Yellow
+            }
+            else {
+                Write-Host "$([char]0x25A0)" -NoNewline -ForegroundColor Red
+            } 
             # Smaller number, faster animation
             Start-Sleep -Milliseconds 5
         }
         # Calculates how much free space at end of the graph
-        $spaceLeft = ((100/100)*30) - $tick
-        # Tracks empty space to draw to push end of graph
-        $add
+        $spaceLeft = ((1)*30) - $tick
         # Loop draws empty space and end of graph
         While($add++ -le $spaceLeft){
-        Write-Host ' ' -NoNewline
+            Write-Host ' ' -NoNewline
         }
         Write-Host ']'
         # Makes space between reports
         "`n"
     }
-    #Calls bar graph function
-    drawBar
+    $myDisk = Get-WmiObject -class win32_logicaldisk
+}
+
+Process {
+    $myDisk | ForEach-Object {
+        if ($_.Drivetype -ne 4){
+            echo 'test'
+
+        $TotalSpace = [Math]::Round($_.Size / 1gb)
+        $FreeSpace = [Math]::Round($_.Freespace / 1gb)
+        $UsedSpace = [Math]::Round(($_.Size - $_.FreeSpace) / 1gb)
+        $PercentUsed = [Math]::Round((($_.Size - $_.FreeSpace) / $_.Size) * 100)
+        $PercentFree = [Math]::Round(($_.FreeSpace / $_.Size) * 100)
+        
+        $param = @{
+            Volume = $_.VolumeName
+            DriveLetter = $_.DeviceID
+            TotalSpace = "$TotalSpace GB"
+            UsedSpace = "$UsedSpace GB"
+            FreeSpace = "$FreeSpace GB"
+            PercentUsed = "$PercentUsed %"
+            PercentFree = "$PercentFree %"
+        }
+        $Results = New-Object -TypeName PSObject -Property $param
+        $Results | ft
+        Get-BarGraph $Results.PercentUsed.Replace('%', '')
+    }
+    }
 }
