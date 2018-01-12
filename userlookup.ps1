a34fff7959adbf25a448a104896e153048400aaf
slusho <willracerx@gmail.com>
2018-01-12 10:48:59 -0600

    Added userlookup.ps1 Simple script to look up user info in AD from PS without opening AD Console

---
+++ userlookup.ps1
@@ -0,0 +1,20 @@
+﻿#Gets User display name, description, email address, and telephone number.
+$User = Read-Host 'Employee Number '
+$StrFilter = "(&(objectCategory=User)(Description=$User))"
+$ObjDomain = New-Object System.DirectoryServices.DirectoryEntry
+
+$ObjSearcher = New-Object System.DirectoryServices.DirectorySearcher
+$ObjSearcher.SearchRoot = $ObjDomain
+$ObjSearcher.PageSize = 1000
+$ObjSearcher.Filter = $StrFilter
+$ObjSearcher.SearchScope = "Subtree"
+
+$ColPropList = @("name", "description", "mail", "telephoneNumber")
+
+$ColPropList | ForEach-Object {$ObjSearcher.PropertiesToLoad.Add($_)}
+$ColResults = $ObjSearcher.FindAll()
+
+$ColResults | ForEach-Object {
+    $ObjItem = $_.Properties 
+    Write-Output $ObjItem
+}
